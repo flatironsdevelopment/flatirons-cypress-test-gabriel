5f7d4a5b31c6dd6c7a39b9497cb22bdcb7ae15aa
Gabriel Siqueira <gabrielleandrojunior@live.com>
2024-11-12 11:52:21 -0300

    feature: added test example

--- cypress/e2e/sample-test.spec.cy.js
+++ cypress/e2e/sample-test.spec.cy.js
@@ -3,7 +3,16 @@ describe("template spec", () => {
     cy.visit("https://staging-fuse-aws.flatirons.com");
   });
 
-  it("Does not do much!", () => {
-    expect(true).to.equal(true);
+  it("Sign up", () => {
+    cy.visit("https://staging-fuse-aws.flatirons.com");
+    cy.contains("Sign Up").click();
+    const random = Math.random().toString(36).substring(7);
+    cy.get("input[id='first_name']").type("Test");
+    cy.get("input[id='last_name']").type("Test");
+    cy.get("input[id='email']").type("test" + random + "@test.com");
+    cy.get("input[id='password']").type("Password@2024");
+    cy.get("input[id='company_name']").type("Test");
+    cy.contains("Sign Up").click();
+    cy.contains("Check your email").should("be.visible");
   });
 });
